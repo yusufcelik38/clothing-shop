8d0668a1dc8f51ff7f3def9394c8e4343e6a66bf
YUSUF CELIK <yusufcelik38@gmail.com>
2020-08-24 20:48:49 +0300

    collection-item component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import './App.css';
-import HomePage from  './pages/homepage/homepage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import { Route, Switch } from "react-router-dom";
+
+
+
+
+
 function App() {
   return (
-  <HomePage/>);
+    <div>
+    <Switch>
+        <Route exact  path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        
+        </Switch>
+     </div>);
 }
 
 export default App;
